d99931c5540f6845e32ddfd120d8855d09da46fd
apovn <tritranduc@gmail.com>
2018-03-19 10:12:15 +0700

    update code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,10 @@ public class Main {
         try {
             EchoBot echo = new EchoBot();
             botsApi.registerBot(echo);
+//            echo.sendMessageToGroup(-297605950l, "Hi, I'm ET_BOT.");  // Group
+            echo.sendMessageToGroup(-1001312008375l, "Hi channel members. I'm BOT."); // Public Channel: https://web.telegram.org/#/im?p=@etalertchannel
+            echo.sendMessageToGroup(-1001331863204l, "Hi channel members. I'm BOT."); // Private Channel: https://web.telegram.org/#/im?p=c1331863204_15226261375188193354
+//            echo.sendMessageToUser(494637284l, "Xin chào bạn!");
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
